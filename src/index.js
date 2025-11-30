832792ce46dc38b573fff845327167e83ab5e9cc
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-30 18:00:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-body {
-  background-color: #141414;
-  color: white;
-  overflow-x: hidden;
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
 
-/* Hide scrollbar for consistent UI */
-.no-scrollbar::-webkit-scrollbar {
-  display: none;
-}
-.no-scrollbar {
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
