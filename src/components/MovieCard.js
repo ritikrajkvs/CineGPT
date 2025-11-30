e581efc4fdb26d836253baab756ef3484c299ea2
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-30 17:58:05 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,12 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 flex-shrink-0">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 md:w-48 pr-4 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:z-20 origin-center">
+      <img
+        alt="Movie Card"
+        src={IMG_CDN_URL + posterPath}
+        className="rounded-md object-cover shadow-lg drop-shadow-md"
+      />
     </div>
   );
 };
