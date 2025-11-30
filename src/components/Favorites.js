eac1dbccd424c0cd4f6bac2245f11956281bc72d
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-30 22:03:44 +0530

    Update Favorites.js

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Header from './Header';
 import { BG_URL } from '../utils/constants';
-
-// OPTIMIZATION: Move lazy import outside component to prevent re-creation on every render
-const MovieCard = React.lazy(() => import('./MovieCard')); 
+import MovieCard from './MovieCard'; // Changed to standard import
+import MovieTrailerModal from './MovieTrailerModal'; // Added Modal
 
 const Favorites = () => {
   const favorites = useSelector((store) => store.movies.favorites);
@@ -30,14 +29,14 @@ const Favorites = () => {
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
                 {favorites.map((movie) => (
                     <div key={movie.id} className="w-full">
-                        <React.Suspense fallback={<div className="text-white">Loading...</div>}>
-                            <MovieCard movie={movie} />
-                        </React.Suspense>
+                        <MovieCard movie={movie} />
                     </div>
                 ))}
             </div>
           )}
         </div>
+        {/* The Modal was missing here! */}
+        <MovieTrailerModal />
       </div>
     </>
   );
